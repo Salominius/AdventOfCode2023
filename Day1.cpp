2adbe8506f25cb7db9b74fde1a6bdb13bd3e8fd8
Arne Malaka <a12028537@unet.univie.ac.at>
2023-12-01 07:18:36 +0100

    First solution. Will improve later

--- Day1.cpp
+++ Day1.cpp
@@ -1,9 +1,53 @@
 #include "helpers/inputHelpers.hpp"
 
 int main() {
-  int part1 = 0;
+  uint64_t part1 = 0;
+  uint64_t part2 = 0;
 
   std::vector<std::string> input = getInput<std::string>("inputs/day1.txt");
 
-  
+  for (const std::string& line : input) {
+    uint8_t firstDigit = 0;
+    uint8_t lastDigit = 0;
+    for (const char& c : line){
+      //check if c is int:
+      if (c >= '0' && c <= '9') {
+        if (firstDigit == 0) {
+          firstDigit = c - '0';
+        }
+        lastDigit = c - '0';
+      }
+    }
+    part1 += firstDigit * 10 + lastDigit;
+
+    firstDigit = 0;
+    lastDigit = 0;
+
+    // Tried an eternity with regex (complex because of overlapping matches and c++ stuff)
+    // Only to find out that c++ regex has terrible performance anyway
+    std::string digitStrings[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    for (uint8_t i = 0; i < line.length(); i++) {
+      if (line[i] >= '0' && line[i] <= '9') {
+        if (firstDigit == 0) {
+          firstDigit = line[i] - '0';
+        }
+        lastDigit = line[i] - '0';
+      }
+      else {
+        for (uint8_t d = 0; d < 9; d++) {
+          if (line.substr(i, digitStrings[d].size()) == digitStrings[d]) {
+            if (firstDigit == 0) {
+              firstDigit = d + 1;
+            }
+            lastDigit = d + 1;
+          }
+        }
+      }
+    }
+
+    part2 += firstDigit * 10 + lastDigit;
+  }
+
+  std::cout << "Part 1: " << part1 << std::endl;
+  std::cout << "Part 2: " << part2 << std::endl; //54203
 }
