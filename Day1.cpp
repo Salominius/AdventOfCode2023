036c169a116cfdf644ad2d977d690cfadff1f183
Arne Malaka <a12028537@unet.univie.ac.at>
2023-12-01 11:29:55 +0100

    Day 1

--- Day1.cpp
+++ Day1.cpp
@@ -7,12 +7,14 @@ int main() {
   std::vector<std::string> input = getInput<std::string>("inputs/day1.txt");
 
   for (const std::string& line : input) {
+    //Part 1
     uint8_t firstDigit = 0;
     uint8_t lastDigit = 0;
     for (const char& c : line){
       //check if c is int:
       if (c >= '0' && c <= '9') {
         if (firstDigit == 0) {
+          //Set first digit only if not set
           firstDigit = c - '0';
         }
         lastDigit = c - '0';
@@ -20,11 +22,12 @@ int main() {
     }
     part1 += firstDigit * 10 + lastDigit;
 
+    //Part 2
     firstDigit = 0;
     lastDigit = 0;
 
     // Tried an eternity with regex (complex because of overlapping matches and c++ stuff)
-    // Only to find out that c++ regex has terrible performance anyway
+    // Only to find out that c++ regex has terrible performance anyway and is commenly called "broken garbage"
     std::string digitStrings[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     for (uint8_t i = 0; i < line.length(); i++) {
       if (line[i] >= '0' && line[i] <= '9') {
@@ -48,6 +51,6 @@ int main() {
     part2 += firstDigit * 10 + lastDigit;
   }
 
-  std::cout << "Part 1: " << part1 << std::endl;
+  std::cout << "Part 1: " << part1 << std::endl; //54667
   std::cout << "Part 2: " << part2 << std::endl; //54203
 }
