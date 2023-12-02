15d252938af22ce68b98d167d377eeb40c61d0dc
Arne Malaka <a12028537@unet.univie.ac.at>
2023-12-02 12:56:15 +0100

    prettier version

--- Day2.cpp
+++ Day2.cpp
@@ -1,83 +1,43 @@
 #include "helpers/inputHelpers.hpp"
+#include <sstream>
 
 int main() {
-  uint64_t part1 = 0;
-  const uint8_t maxRed = 12;
-  const uint8_t maxGreen = 13;
-  const uint8_t maxBlue = 14; 
+  int part1 = 0;
+  const int maxRed = 12;
+  const int maxGreen = 13;
+  const int maxBlue = 14;
 
-  uint64_t part2 = 0;
+  int part2 = 0;
 
-  std::vector<std::string> input = getInput("inputs/day2.txt");
+  for (const std::string& line : getInput("inputs/day2.txt")) {
+    int fewestPossibleRed = 0;
+    int fewestPossibleGreen = 0;
+    int fewestPossibleBlue = 0;
 
-  for (const std::string& line : input) {
-    // Part 2 variables per game:
-    uint16_t fewestPossibleRed = 0;
-    uint16_t fewestPossibleGreen = 0;
-    uint16_t fewestPossibleBlue = 0;
+    std::istringstream iss(line);
+    std::string game;
+    std::string colon = ":";
+    int id;
+    iss >> game >> id >> colon;
 
-    // start at 5 to skip the "Game " part
-    uint8_t i = 5;
-
-    // parse the id
-    uint8_t id = 0;
-    for (; line[i] >= '0' && line[i] <= '9'; i++) {
-      id *= 10;
-      id += line[i] - '0';
-    }
-    
-    //skip to the first number
-    i += 2;
-    while (i < line.length()) {
-      // parse the amount drawn
-      uint8_t amountDrawn = 0;
-      for (;line[i] >= '0' && line[i] <= '9'; i++) {
-        amountDrawn *= 10;
-        amountDrawn += line[i] - '0';
-      }
-
-      // skip the ' ' after the number
-      if (line[i] == ' ') {
-        i++;
-      }
-
-      // parse the color
-      switch (line[i]) {
-        case 'g':
-          //Part 1
-          if (amountDrawn > maxGreen) {
-            id = 0;
-          } else {
-            i += 7; //skip to the next number
-          }
-          //Part 2
-          if (amountDrawn > fewestPossibleGreen) {
-            fewestPossibleGreen = amountDrawn;
-          }
-          break;
+    int amount;
+    std::string color;
+    while (iss >> amount >> color) {
+      switch(color[0]) {
         case 'r':
-          //Part 1
-          if (amountDrawn > maxRed) {
+          if (amount > maxRed)
             id = 0;
-          } else {
-            i += 5; //skip to the next number
-          }
-          //Part 2
-          if (amountDrawn > fewestPossibleRed) {
-            fewestPossibleRed = amountDrawn;
-          }
+          fewestPossibleRed = std::max(fewestPossibleRed, amount);
+          break;
+        case 'g':
+          if (amount > maxGreen)
+            id = 0;
+          fewestPossibleGreen = std::max(fewestPossibleGreen, amount);
           break;
         case 'b':
-          //Part 1
-          if (amountDrawn > maxBlue) {
+          if (amount > maxBlue)
             id = 0;
-          } else {
-            i += 6; //skip to the next number
-          }
-          //Part 2
-          if (amountDrawn > fewestPossibleBlue) {
-            fewestPossibleBlue = amountDrawn;
-          }
+          fewestPossibleBlue = std::max(fewestPossibleBlue, amount);
           break;
       }
     }
