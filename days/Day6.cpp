e3ba38d658dc84e666f6da391bf2e503f34ad8cf
Arne Malaka <a12028537@unet.univie.ac.at>
2023-12-06 12:17:43 +0100

    Day 6

---
+++ days/Day6.cpp
@@ -0,0 +1,60 @@
+#include "../helpers/inputHelpers.hpp"
+#include <sstream>
+#include <math.h>
+
+int getNumberOfWaysToWin(double totalTime, double highscore) {
+  // distance = speed * time   | speed = wait and time = totalTime - wait
+  // distance = wait * (totalTime - wait)
+  // distance = wait*totalTime - wait^2   | - distance
+  // 0 = - wait^2 + wait*totalTime - distance   | * -1
+  // 0 = wait^2 - wait*totalTime + distance   | solve for wait (pq-formula) to know how long one has to wait to reach the given distance
+  // wait = totalTime/2 +- sqrt((totalTime/2)^2 - distance)   (pq-formula)
+  double distance = highscore + 0.001; // + 0.001 to make sure that the distance is higher than the highscore
+  int minWait = std::ceil(totalTime/2 - sqrt((totalTime/2)*(totalTime/2) - distance));
+  int maxWait = std::floor(totalTime/2 + sqrt((totalTime/2)*(totalTime/2) - distance));
+  return maxWait - minWait + 1;
+}
+
+int main() {
+  int part1 = 1;
+  int part2 = 0;
+
+  std::vector<std::string> input = getInput("../inputs/day6.txt");
+  
+  //Get input for Part 1:
+  std::vector<int> times;
+  std::vector<int> highscores;
+  int number;
+  
+  std::istringstream iss(input[0].substr(5));
+  while (iss >> number)
+    times.push_back(number);
+
+  iss = std::istringstream(input[1].substr(9));
+  while (iss >> number)
+    highscores.push_back(number);
+
+  //Part 1:
+  for (int i = 0; i < times.size(); i++)
+    part1 *= getNumberOfWaysToWin(times[i], highscores[i]);
+
+  //Get input for Part 2:
+  std::string totalTimeString, highscoreString;
+
+  for (char c : input[0].substr(5))
+    if (c != ' ')
+      totalTimeString += c;
+  double totalTime = std::stod(totalTimeString);
+
+  for (char c : input[1].substr(9))
+    if (c != ' ')
+      highscoreString += c;
+  double highscore = std::stod(highscoreString);
+
+  //Part 2:
+  std::cout << totalTime << " " << highscore << std::endl;
+  part2 = getNumberOfWaysToWin(totalTime, highscore);
+
+  std::cout << "Part 1: " << part1 << std::endl; //741000
+  std::cout << "Part 2: " << part2 << std::endl; //
+}
