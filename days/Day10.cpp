ccc8985661ff725f875c5ee9ce8b19430023c887
Arne Malaka <a12028537@unet.univie.ac.at>
2023-12-11 16:52:43 +0100

    Day 10

--- days/Day10.cpp
+++ days/Day10.cpp
@@ -2,6 +2,7 @@
 #include "../helpers/lists.hpp"
 #include "../helpers/math.hpp"
 #include <tuple>
+#include <set>
 
 enum Direction { UP, RIGHT, DOWN, LEFT };
 
@@ -72,20 +73,65 @@ int main() {
   }
   // At least 2 pipes have to end in S, so by checking 3/4 directions we can be certain to have found a valid pipe
 
+  Direction startDirection = direction; // Save start direction for part 2
+
   size_t steps = 1;
   auto[x, y] = getNextPosition(startX, startY, direction); // x, y are now the position of the first pipe
   pipe = grid[y][x];
+
+  std::set<std::pair<int, int>> mainLoop = {std::make_pair(x, y)}; // Find positions of main loop for part 2
 
   // Follow the pipe until we reach the end
   while (pipe != 'S') {
     steps++;
     direction = getNextDirection(pipe, direction);
     std::tie(x, y) = getNextPosition(x, y, direction);
+    mainLoop.insert(std::make_pair(x, y)); // Find positions of main loop for part 2
     pipe = grid[y][x];
   }
 
+  Direction endDirection = direction; // Save end direction for part 2
+
   part1 = steps/2;
+
+  // A bit messy, but we need to know what kind of pipe S is for the following algorithm to work
+  if ((startDirection == RIGHT && endDirection == DOWN) || (startDirection == UP && endDirection == LEFT))
+    grid[startY][startX] = 'L';
+  else if ((startDirection == RIGHT && endDirection == UP) || (startDirection == DOWN && endDirection == LEFT))
+    grid[startY][startX] = 'F';
+  else if ((startDirection == LEFT && endDirection == DOWN) || (startDirection == UP && endDirection == RIGHT))
+    grid[startY][startX] = 'J';
+  else if ((startDirection == LEFT && endDirection == UP) || (startDirection == DOWN && endDirection == RIGHT))
+    grid[startY][startX] = '7';
+  else if (startDirection == RIGHT)
+    grid[startY][startX] = '-';
+  else
+    grid[startY][startX] = '|';
+
+  // Part 2
+  for (size_t y = 0; y < grid.size(); y++) {
+    bool inLoop = false; // Left border can't be enclosed by loop
+    char previous = ' '; // Track L-7 and F-J which should count as |  (just one toggle of inLoop)
+    for (size_t x = 0; x < grid[y].size(); x++) {
+      if (!mainLoop.count(std::make_pair(x, y))) {
+        if (inLoop)
+          part2++; // count tile if inLoop and not part of the loop itself
+        continue;
+      }
+
+      char tile = grid[y][x];
+      if (tile == '|')
+        inLoop = !inLoop;
+      else if (tile == 'L' || tile == 'F')
+        previous = tile;
+      else if (tile == '7' || tile == 'J')
+        if (previous + tile == 'L' + '7' || previous + tile == 'F' + 'J') // L-7 and F-J should count as |
+          inLoop = !inLoop;
+        else
+          previous = ' '; // Reset previous if not L-7 or F-J
+    }
+  }
   
   std::cout << "Part 1: " << part1 << std::endl; //6725
-  std::cout << "Part 2: " << part2 << std::endl; //
+  std::cout << "Part 2: " << part2 << std::endl; //383
 }
